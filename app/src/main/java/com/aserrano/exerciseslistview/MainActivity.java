bd0ba96c21e88df7ec4b3ae19c86e05bd39471d5
DAM <aserrano@cifpfbmoll.eu>
2022-01-28 17:27:36 +0100

    Second commit

--- app/src/main/java/com/aserrano/exerciseslistview/MainActivity.java
+++ app/src/main/java/com/aserrano/exerciseslistview/MainActivity.java
@@ -5,33 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.aserrano.exerciseslistview.R;
+
 public class MainActivity extends ListActivity {
-
-    private static final String[] items ={"lorem", "ipsum", "dolor",
-            "sit", "amet", "consectetuer", "adipiscing", "elit",
-            "morbi", "vel", "ligula", "vitae", "arcu", "aliquet",
-            "mollis", "etiam", "vel", "erat", "placerat", "ante",
+    TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
             "porttitor", "sodales", "pellentesque", "augue", "purus"};
-    private TextView selector;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        setListAdapter(new SpecialAdapter());
+        selection=(TextView)findViewById(R.id.selection);
+        }
+    public void onListItemClick(ListView parent, View v,
+        int position, long id) {
+        selection.setText(items[position]);
+        }
 
-        setListAdapter(new ArrayAdapter<String>(this,
-                R.layout.row, R.id.textViewRow, items));
+    class SpecialAdapter extends ArrayAdapter<String> {
+        SpecialAdapter() {
+            super(MainActivity.this, R.layout.row, R.id.textViewRow, items);
+        }
 
-        selector = findViewById(R.id.selection);
+        public View getView(int position, View convertView,
+                        ViewGroup parent) {
+        View row=super.getView(position, convertView, parent);
+        ImageView icon=(ImageView)row.findViewById(R.id.imageViewRow);
+        if (items[position].length()>4) {
+            icon.setBackgroundResource(R.drawable.delete);
+        }
+        else {
+            icon.setBackgroundResource(R.drawable.check);
+        }
+        return(row);
+        }
     }
-
-    public void onListItemClick(ListView parent, View v, int position, long id){
-        selector.setText(items[position]);
-    }
-
-
 }
